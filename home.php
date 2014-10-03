05a9e5af39646befd97bf41c74381898b2592eca
nicolasfavier <niko.favier@gmail.com>
2014-10-03 17:34:55 +0200

    tpl for the tab

--- home.php
+++ home.php
@@ -14,47 +14,7 @@
 	?>
 		<div class="col-sm-9">
 			<h1>Hello</h1>
-			<table class="table table-bordered table-hover">
-		      <thead>
-		        <tr>
-		          <th>test Name</th>		        
-		          <th>First Name</th>
-		          <th>Last Name</th>
-		          <th>Username</th>
-		          <th>#</th>
-		        </tr>
-		      </thead>
-		      <tbody>
-		        <tr class="info">
-		          <td><input class="form-control" ></td>
-		          <td><input class="form-control"></td>
-		          <td><input class="form-control"></td>
-		          <td><input class="form-control"></td>
-		          <td><button type="button" class="btn btn-info">search</button></td>		          
-		        </tr>
-		        <tr>
-		          <td>Mark</td>		        
-		          <td>Mark</td>
-		          <td>Otto</td>
-		          <td>@TwBootstrap</td>
-		          <td>1</td>
-		        </tr>
-		        <tr>
-          		  <td>Mark</td>		        
-		          <td>Mark</td>
-		          <td>Otto</td>
-		          <td>@TwBootstrap</td>
-		          <td>2</td>		          
-		        </tr>
-		        <tr>
-          		  <td>Mark</td>		        
-		          <td>Mark</td>
-		          <td>Otto</td>
-		          <td>@TwBootstrap</td>
-		          <td>3</td>
-		        </tr>
-		      </tbody>
-		    </table>
+		 	<?php $smarty->display("tpl/pathologytab.tpl"); ?>
 	    </div><!-- ./col-sm-9 -->
  	<?php 
  		$smarty->display("tpl/footer.tpl"); 
---
+++ templates_c/2c984e4cf760fcf082aa8a081be392a4c9803905.file.pathologytab.tpl.php
@@ -0,0 +1,62 @@
+<?php /* Smarty version Smarty-3.1.19, created on 2014-10-03 17:29:06
+         compiled from "tpl\pathologytab.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:4072542ec0c2600d27-85642176%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '2c984e4cf760fcf082aa8a081be392a4c9803905' => 
+    array (
+      0 => 'tpl\\pathologytab.tpl',
+      1 => 1412350141,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '4072542ec0c2600d27-85642176',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_542ec0c2626d20_44447102',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_542ec0c2626d20_44447102')) {function content_542ec0c2626d20_44447102($_smarty_tpl) {?><table class="table table-bordered table-hover">
+		      <thead>
+		        <tr>
+		          <th>test Name</th>		        
+		          <th>First Name</th>
+		          <th>Last Name</th>
+		          <th>Username</th>
+		          <th>#</th>
+		        </tr>
+		      </thead>
+		      <tbody>
+		        <tr class="info">
+		          <td><input class="form-control" ></td>
+		          <td><input class="form-control"></td>
+		          <td><input class="form-control"></td>
+		          <td><input class="form-control"></td>
+		          <td><button type="button" class="btn btn-info">search</button></td>		          
+		        </tr>
+		        <tr>
+		          <td>Mark</td>		        
+		          <td>Mark</td>
+		          <td>Otto</td>
+		          <td>@TwBootstrap</td>
+		          <td>1</td>
+		        </tr>
+		        <tr>
+          		  <td>Mark</td>		        
+		          <td>Mark</td>
+		          <td>Otto</td>
+		          <td>@TwBootstrap</td>
+		          <td>2</td>		          
+		        </tr>
+		        <tr>
+          		  <td>Mark</td>		        
+		          <td>Mark</td>
+		          <td>Otto</td>
+		          <td>@TwBootstrap</td>
+		          <td>3</td>
+		        </tr>
+		      </tbody>
+</table><?php }} ?>
