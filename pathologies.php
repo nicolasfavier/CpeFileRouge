e5f5237aae3fd599aafd625f4f67bba98cb33460
bert <flavien@bert-laptop-mint.(none)>
2014-10-09 09:59:27 +0200

    add the pathologie tpl

--- pathologies.php
+++ pathologies.php
@@ -22,7 +22,10 @@
 	));
 
 	$smarty->display("tpl/topNavBar.tpl");
-	$smarty->display("tpl/nav.tpl");		
+	$smarty->display("tpl/nav.tpl");	
+
+	//$smarty->display("tpl/pathologie.tpl");	
+
 	$smarty->display("tpl/thead.tpl");
 
 	if(isset($_SESSION["User"])){
--- serveur/dao/pathologieDao.php
+++ serveur/dao/pathologieDao.php
@@ -1,67 +1,96 @@
 <?php 
 
-	include_once('../config/dbConfig.php');
+//include_once("../models/Meridien.class.php");
+//include_once("../models/Pathologie.class.php");
+include_once('serveur/config/dbConfig.php');
 
-	function getTbale( $searchpatho, $searchtype, $searchmeridien){
-		$bdd = getDB('filerouge');
-		$res = null;
 
-		try{
+function getTbaleFromAutocomplete(){
 
-			$condition = "SELECT patho.desc, patho.type, meridien.nom, patho.idP  FROM meridien, patho WHERE meridien.code = patho.mer";
+	$keyWords = "";
 
-			if (!empty($searchpatho)){
-				$condition = $condition." AND patho.desc like '%" . $searchpatho."%'";
-			}
+	if(isset($_POST['keyWords']) ){
+		$keyWords = trim($_POST['keyWords']);		
+	}	
 
-			if (!empty($searchtype)){
-				$condition = $condition." AND patho.type like '%" . $searchtype."%'";
-			}
+	return getResearchResultFromKeyWords($keyWords);
+}
+function getTbale(){
 
-			if (!empty($searchmeridien)){
-				$condition = $condition." AND meridien.nom like '%" . $searchmeridien."%'";
-			}
+	$searchpatho = "";
+	$searchtype = "";
+	$searchmeridien = "";
 
-			$req = $bdd->prepare($condition);
+	if(isset($_POST['searchpatho']) && isset($_POST['searchtype']) && isset($_POST['searchmeridien'])){
+		$searchpatho = trim($_POST['searchpatho']);
+		$searchtype = trim($_POST['searchtype']);
+		$searchmeridien = trim($_POST['searchmeridien']);
+	}	
 
-			$req->execute();	
+	return getResearchResult( $searchpatho, $searchtype, $searchmeridien);
+}
 
-			$objreturn = $req->fetchAll();
+function getResearchResult( $searchpatho, $searchtype, $searchmeridien){
+	$bdd = getDB('filerouge');
+	$res = null;
 
-			if( $objreturn ) {
-				$res = $objreturn;
-			} 
-			return $res;
+	try{
+
+		$condition = "SELECT patho.desc, patho.type, meridien.nom  FROM meridien, patho WHERE meridien.code = patho.mer";
+
+		if (!empty($searchpatho)){
+			$condition = $condition." AND patho.desc like '%" . $searchpatho."%'";
 		}
-		catch(PDOException $e){
-			die('Erreur: '.$e->getMessage());
+
+		if (!empty($searchtype)){
+			$condition = $condition." AND patho.type like '%" . $searchtype."%'";
 		}
+
+		if (!empty($searchmeridien)){
+			$condition = $condition." AND meridien.nom like '%" . $searchmeridien."%'";
+		}
+
+		$req = $bdd->prepare($condition);
+
+		$req->execute();	
+
+		$objreturn = $req->fetchAll();
+
+		if( $objreturn ) {
+			$res = $objreturn;
+		} 
+		return $res;
 	}
-
-
-	function getResearchResultFromKeyWords( $keyWords){
-		$bdd = getDB('filerouge');
-		$res = null;
-
-		try{
-
-			$condition = "SELECT patho.desc,patho.type, meridien.nom,  patho.idP FROM keySympt,keywords,symptome, symptPatho,patho,meridien WHERE keywords.idK = keySympt.idK AND keywords.name like :keywords and symptome.idS = keySympt.idS and symptPatho.idS = symptome.idS AND symptPatho.idP = patho.idP AND meridien.code = patho.mer"; 
-
-			$req = $bdd->prepare($condition);
-
-			$req->execute(array(
-			    'keywords' => "%".$keyWords."%"
-			    ));	
-			$objreturn = $req->fetchAll();
-
-			if( $objreturn ) {
-				$res = $objreturn;
-			} 
-			return $res;
-		}
-		catch(PDOException $e){
-			die('Erreur: '.$e->getMessage());
-		}
+	catch(PDOException $e){
+		die('Erreur: '.$e->getMessage());
 	}
+}
+
+
+function getResearchResultFromKeyWords( $keyWords){
+	$bdd = getDB('filerouge');
+	$res = null;
+
+	try{
+
+		$condition = "SELECT patho.desc, patho.type, meridien.nom  FROM meridien, patho WHERE meridien.code = patho.mer";
+		
+		$req = $bdd->prepare($condition);
+
+		$req->execute(array(
+		    'keywords' => "%".$keyWords."%"
+		    ));	
+		$objreturn = $req->fetchAll();
+
+		if( $objreturn ) {
+			$res = $objreturn;
+		} 
+		return $res;
+	}
+	catch(PDOException $e){
+		die('Erreur: '.$e->getMessage());
+	}
+}
+
 ?>
 
